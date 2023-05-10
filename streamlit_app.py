6fe5059aafffd4b29a698bb07a915611bc2bc8c9
michalina-nestorowicz <118530958+michalina-nestorowicz@users.noreply.github.com>
2023-05-10 08:39:34 +0200

    Added dataframe read from S3
    added pandas library that read file from http S3 bucket
    Added streamlit dataframe off all the available fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Healthy Diner')
-   
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -9,3 +11,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocade Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)

cad0f7bd63218e1d6b161fdb3d05538475f9d3de
michalina-nestorowicz <118530958+michalina-nestorowicz@users.noreply.github.com>
2023-05-10 11:50:44 +0200

    show fruityvice response as df

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# response to pandas df 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# show fruityvice df
+streamlit.dataframe(fruityvice_normalized)
+
 
